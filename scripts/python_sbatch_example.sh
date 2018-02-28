9c026e8bb76c93f3e4c320ce98721205329f1087
bohaohuang <bohao.huang@duke.edu>
2018-02-28 13:55:19 -0500

    dcc cluster update

--- scripts/python_sbatch_example.sh
+++ scripts/python_sbatch_example.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#SBATCH -e slurm.err                         # error message will be stored in slurm.err
-#SBATCH --mem=20G                            # request 20G for ram
-#SBATCH -c 6                                 # 6 cpu cores
-#SBATCH -p gpu-common --gres=gpu:1           # request for 1 gpu
-export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib:$LD_LIBRARY_PATH
-python mycode.py > file.out
+#SBATCH -e slurm.err                                                   # error log file
+#SBATCH --mem=20G                                                      # request 20G memory
+#SBATCH -c 6                                                           # request 6 gpu cores
+#SBATCH --exclude=dcc-gpu-[31-32]                                      # exclude GPUs (those two have less than 4G RAM)
+#SBATCH -p gpu-common --gres=gpu:1                                     # request 1 gpu for this job
+module load Anaconda3/3.5.2                                            # load conda to make sure use GPU version of tf
+# add cuda and cudnn path
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
+# add my library path
+export PYTHONPATH=$PYTHONPATH:/dscrhome/bh163/code/uab
+# execute my file
+python train_spca_unet_psbs.py
